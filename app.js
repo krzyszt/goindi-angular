4d4239ed200c9e5b1423ceede35b4e685724b053
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-12-10 11:09:17 +0000

    yo angular

--- app.js
+++ app.js
@@ -5,25 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser("goindi angular cookie"));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(require('less-middleware')(path.join(__dirname, 'app')));
+app.use(express.static(path.join(__dirname, 'app')));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
